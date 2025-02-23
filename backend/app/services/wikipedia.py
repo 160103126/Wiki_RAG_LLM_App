d72d20baa92c44f802bb4718d06267ce3756802c
160103126 <pradeeplachireddy@gmail.com>
2025-02-23 19:27:22 +0530

    wiki app refined

--- backend/app/services/wikipedia.py
+++ backend/app/services/wikipedia.py
@@ -1,6 +1,43 @@
 import wikipediaapi
 import wikipedia
 import requests
+from spellchecker import SpellChecker
+import re
+import spacy
+
+
+def clean_query(query):
+    # Remove special characters and normalize whitespace
+    cleaned = re.sub(r"[^a-zA-Z0-9\s]", "", query)
+    cleaned = " ".join(cleaned.split()).lower()
+    return cleaned
+
+
+nlp = spacy.load("en_core_web_sm")
+
+def preprocess_text(text):
+    """Removes stop words and applies lemmatization."""
+    doc = nlp(text)
+    
+    clean_tokens = [
+        token.lemma_ for token in doc 
+        if not token.is_stop and not token.is_punct
+    ]
+    
+    return " ".join(clean_tokens)
+
+
+def get_search_suggestions(query):
+    url = "https://en.wikipedia.org/w/api.php"
+    params = {
+        "action": "opensearch",
+        "search": query,
+        "limit": 5,
+        "namespace": 0,
+        "format": "json"
+    }
+    response = requests.get(url, params=params).json()
+    return response[1][0]  # Returns list of suggested titles
 
 def search_wikipedia(query: str, lang: str = "en", max_results: int = 3):
     """Search Wikipedia and return multiple relevant page summaries."""
@@ -41,4 +78,10 @@ def search_wikipedia(query: str, lang: str = "en", max_results: int = 3):
     return results
 
 if __name__=="__main__":
-    print(search_wikipedia('Neural Networks'))
+    user_query = "Who is robert downey"
+    # cleaned = clean_query(user_query)  
+    cleaned_text = preprocess_text(user_query)
+    print('cleaned text:',cleaned_text)
+    suggested = get_search_suggestions(cleaned_text)
+    print('suggested:',suggested)
+    print(search_wikipedia(suggested))
